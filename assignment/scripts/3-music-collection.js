ccbf475b5135e6785a5af0a11c66f9fa509cfc3c
Meleah Roe <melroe19@gmail.com>
2024-06-30 17:55:42 -0500

    added albums to array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -5,21 +5,80 @@ console.log('in my collection', myCollection);
 const collection =[];
 console.log('in collection', collection);
 
-
 function addToCollection(collection, title, artist, yearPublished) {
-
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  };
+  collection.push(album);
+  return album;
 }
 
+let album1 = {
+  title: "Keep It Simple",
+  artist: "Eli Mattson",
+  yearPublished: 2019
+};
+let album2 = {
+  title: "Sigh No More",
+  artist: "Mumford & Sons",
+  yearPublished: 2009
+}
+let album3 = {
+  title: "It Means Everything",
+  artist: "Save Ferris",
+  yearPublished: 1997
+}
+let album4 = {
+  title: "Some Nights",
+  artist: "Fun",
+  yearPublished: 2012
+}
+let album5 = {
+  title: "Hard Candy",
+  artist: "Counting Crows",
+  yearPublished: 2002
+}
+let album6 = {
+  title: "Invisible Touch",
+  artist: "Genesis",
+  yearPublished: 1986
+}
+let album7 = {
+  title: "Brand New",
+  artist: "Ben Rector",
+  yearPublished: 2015
+}
 
-//   function collection (title, artist, yearPublished){
-//   Const album = { 
-//   let album.title = "title";
-//   let album.artist = "artist;,
-//   let album.yearPublished = 'yearPublished'}
-//   }
-//     console.log(myCollection, title, artist, yearPublished, collection);
+addToCollection(myCollection, album1.title, album1.artist, album1.yearPublished);
+console.log(myCollection);
+addToCollection(myCollection, album2.title, album2.artist, album2.yearPublished);
+console.log(myCollection);
+addToCollection(myCollection, album3.title, album3.artist, album3.yearPublished);
+console.log(myCollection);
+addToCollection(myCollection, album4.title, album4.artist, album4.yearPublished);
+console.log(myCollection);
+addToCollection(myCollection, album5.title, album5.artist, album5.yearPublished);
+console.log(myCollection);
+addToCollection(myCollection, album6.title, album6.artist, album6.yearPublished);
+console.log(myCollection);
+addToCollection(myCollection, album7.title, album7.artist, album7.yearPublished);
+console.log(myCollection);
 
-// }
+
+
+
+//function addToCollection(collection, title, artist, yearPublished) {
+//myCollection.push(album1,album2, album3,album4, album5, album6, album7);
+//console.log(myCollection);
+//}
+ 
+//return the newly created object.
+//Use and Test the addToCollection function:
+//Add 6 albums to the myCollection array. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+
+
 
 
 
